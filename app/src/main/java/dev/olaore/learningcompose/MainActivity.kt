df25406ee67c53dad7533e9bb0f3a1100fdcbc54
Olaore Fouad <olaore.fouad@globalaccelerex.com>
2021-12-15 11:48:06 +0100

    started out with composelearnt how to use material themes, colors, text, modifiers and lazycolumn to generate lists, very interesting

--- app/src/main/java/dev/olaore/learningcompose/MainActivity.kt
+++ app/src/main/java/dev/olaore/learningcompose/MainActivity.kt
@@ -3,11 +3,27 @@ package dev.olaore.learningcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.olaore.learningcompose.data.SampleData
 import dev.olaore.learningcompose.ui.theme.LearningComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +31,59 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LearningComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                Conversation(messages = SampleData.MESSAGES)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(message: Message) {
+
+    Row(modifier = Modifier.padding(16.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "This is the profile picture image!",
+            modifier = Modifier
+                .size(100.dp)
+                .clip(CircleShape)
+                .border(
+                    border = BorderStroke(1.5.dp, MaterialTheme.colors.secondaryVariant),
+                    shape = CircleShape
+                ),
+            contentScale = ContentScale.Crop,
+            alignment = Center
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Column(modifier = Modifier.align(CenterVertically)) {
+            Text(
+                message.author,
+                color = MaterialTheme.colors.secondary,
+                style = MaterialTheme.typography.body2
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(message.body, style = MaterialTheme.typography.subtitle2)
+        }
+    }
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    LearningComposeTheme {
-        Greeting("Android")
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
     }
 }
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    LearningComposeTheme {
+        Conversation(messages = SampleData.MESSAGES)
+    }
+}
+
+// models
+data class Message(val author: String, val body: String)
---
+++ app/src/main/java/dev/olaore/learningcompose/data/SampleData.kt
@@ -0,0 +1,20 @@
+package dev.olaore.learningcompose.data
+
+import dev.olaore.learningcompose.Message
+
+object SampleData {
+
+    val MESSAGES = listOf(
+        Message("Fouad Olaore", "I like hausa women!"),
+        Message("Damilola Adewale", "Dami is a great designer!"),
+        Message("Folashade Olugbogi", "Another fantastic human being!"),
+        Message("Dan Thomas", "Because I'm listening to him and Craig at the moment"),
+        Message("Dan Thomas", "Because I'm listening to him and Craig at the moment"),
+        Message("Dan Thomas", "Because I'm listening to him and Craig at the moment"),
+        Message("Dan Thomas", "Because I'm listening to him and Craig at the moment"),
+        Message("Dan Thomas", "Because I'm listening to him and Craig at the moment"),
+        Message("Dan Thomas", "Because I'm listening to him and Craig at the moment"),
+        Message("Dan Thomas", "Because I'm listening to him and Craig at the moment")
+    )
+
+}
