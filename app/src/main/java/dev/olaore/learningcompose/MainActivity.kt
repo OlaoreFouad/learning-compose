bb62d86481f336aa23776669b40f64560695d73b
Olaore Fouad <olaore.fouad@globalaccelerex.com>
2021-12-17 21:40:01 +0100

    feature: worked on using a buttonnn

--- app/src/main/java/dev/olaore/learningcompose/MainActivity.kt
+++ app/src/main/java/dev/olaore/learningcompose/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
@@ -90,11 +91,23 @@ fun Conversation(messages: List<Message>) {
     }
 }
 
+@Composable
+fun ClickCounter(clicks: Int, onClick: () -> Unit) {
+    Button(onClick = onClick) {
+        Text(text = "I've been clicked $clicks times today!")
+    }
+}
+
 @Preview
 @Composable
 fun PreviewConversation() {
     LearningComposeTheme {
-        Conversation(messages = SampleData.MESSAGES)
+        var count by remember {
+            mutableStateOf(0)
+        }
+        ClickCounter(clicks = count) {
+            count++
+        }
     }
 }
 
