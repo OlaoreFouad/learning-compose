9176fa180637c79eecb3155ef4573076745fefe2
Olaore Fouad <olaore.fouad@globalaccelerex.com>
2022-01-01 18:29:41 +0100

    feature: finished the jetpack compose basics codelab, nice one

--- app/src/main/java/dev/olaore/learningcompose/ui/components/Home.kt
+++ app/src/main/java/dev/olaore/learningcompose/ui/components/Home.kt
@@ -3,25 +3,52 @@ package dev.olaore.learningcompose.ui.components
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import dev.olaore.learningcompose.R
 import dev.olaore.learningcompose.ui.components.ui.theme.LearningComposeTheme
+import kotlin.math.exp
 
 class Home : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            val names = listOf("Fouad", "Olamide", "Olaore")
+            LearningComposeTheme {
+                var shouldShowOnboarding by rememberSaveable { mutableStateOf(false) }
+
+                if (shouldShowOnboarding) {
+                    HomeScreen {
+                        shouldShowOnboarding = false
+                    }
+                } else {
+                    LazyColumn {
+                        items(items = names) { item ->
+                            Greeting(name = item)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -29,36 +56,44 @@ class Home : ComponentActivity() {
 fun Greeting(name: String) {
 
     var expanded by remember { mutableStateOf(false) }
-    val extraPadding = if (expanded) 48.dp else 0.dp
 
-    Surface(
-        color = MaterialTheme.colors.primary,
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Column {
             Row(
                 modifier = Modifier
                     .padding(24.dp)
+                    .animateContentSize(
+                        spring(
+                            dampingRatio = Spring.DampingRatioMediumBouncy,
+                            stiffness = Spring.StiffnessLow
+                        )
+                    )
             ) {
                 Column(
                     modifier = Modifier
                         .weight(1f)
-                        .padding(bottom = extraPadding)
                 ) {
 
                     Text(text = "Hello,", color = colorResource(id = R.color.white))
                     Text(text = name, color = colorResource(id = R.color.white))
 
+                    if (expanded) {
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(text = stringResource(R.string.lorem_ipsum_text))
+                    }
+
                 }
-                OutlinedButton(
-                    onClick = {
-                        expanded = !expanded
-                    },
-                    modifier = Modifier.background(colorResource(id = R.color.white))
-                ) {
-                    Text(
-                        text = "Show ${if (expanded) "Less" else "More"}",
-                        color = colorResource(id = R.color.purple_500)
+                IconButton(onClick = {
+                    expanded = !expanded
+                }) {
+                    Icon(
+                        imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                        contentDescription = if (expanded) stringResource(R.string.show_less_text) else stringResource(
+                            R.string.show_more_text
+                        )
                     )
                 }
             }
@@ -101,7 +136,7 @@ fun HomeScreen(onContinueClicked: () -> Unit) {
 fun PreviewHome() {
     val names = listOf("Fouad", "Nofiu", "Farhan")
     LearningComposeTheme {
-        var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+        var shouldShowOnboarding by rememberSaveable { mutableStateOf(false) }
 
         if (shouldShowOnboarding) {
             HomeScreen {
--- app/src/main/java/dev/olaore/learningcompose/ui/theme/Color.kt
+++ app/src/main/java/dev/olaore/learningcompose/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Teal200 = Color(0xFF03DAC5)
 
 val WHITE = Color(0xFFFFFFFF)
 val BLACK = Color(0xFF000000)
+
+val Navy = Color(0xFF073042)
+val Blue = Color(0xFF4285F4)
+val LightBlue = Color(0xFFD7EFFE)
+val Chartreuse = Color(0xFFEFF7CF)
